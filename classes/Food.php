618d25d5738e70b059fa53557b75afeb4b3559d5
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-06 23:47:22 +0200

    feat: added the class food

--- classes/Food.php
+++ classes/Food.php
@@ -1,12 +1,45 @@
 <?php
 
 require_once __DIR__ . "/product.php";
-    class Food extends Product{
-        
 
 
+class Food extends Product{
 
+    protected $expirationDate;
+    protected $vegan;
+    protected $ingredients;
+
+    public function getIngredients()
+    {
+        return $this->ingredients;
     }
+
+    public function setIngredients($ingredients)
+    {
+        $this->ingredients = $ingredients;
+    }
+
+
+    public function getVegan()
+    {
+        return $this->vegan;
+    }
+
+    public function setVegan($vegan)
+    {
+        $this->vegan = $vegan;
+    }
+
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+
+    public function setExpirationDate($expirationDate)
+    {
+        $this->expirationDate = $expirationDate;
+    }
+}
 
 
 ?>
